9e56cf09232c8710c84b2fa9ae1666c9c298fbb5
gabrielrics <gabriel908henrique@gmail.com>
2023-09-04 22:34:15 -0300

    Imsersão ao Firebase

--- Iniciando firebase/Atividade/src/main/java/com/example/myapplication/MainActivity.java
+++ Iniciando firebase/Atividade/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,13 +9,23 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseReference minhaReferencia = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+
+        minhaReferencia.child("produtos").child("001").child("descricao").setValue("coca-cola");
+        minhaReferencia.child("produtos").child("001").child("preco").setValue("12.50");
+
+        minhaReferencia.child("produtos").child("002").child("descricao").setValue("suco");
+        minhaReferencia.child("produtos").child("002").child("preco").setValue("4.50");
+
+        minhaReferencia.child("produtos").child("003").child("descricao").setValue("água");
+        minhaReferencia.child("produtos").child("003").child("preco").setValue("6.50");
+
+        minhaReferencia.child("produtos").child("004").child("descricao").setValue("café");
+        minhaReferencia.child("produtos").child("004").child("preco").setValue("7.90");
     }
 }
