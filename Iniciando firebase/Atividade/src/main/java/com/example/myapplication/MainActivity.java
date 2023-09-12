a4f99589a6964d00a1a3d931c0b9ca220f184f6a
gabrielrics <gabriel908henrique@gmail.com>
2023-09-11 22:29:47 -0300

    Firebase

--- Iniciando firebase/Atividade/src/main/java/com/example/myapplication/MainActivity.java
+++ Iniciando firebase/Atividade/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,14 +9,24 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference minhaReferencia = FirebaseDatabase.getInstance().getReference();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //private DatabaseReference minhaReferencia = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+// Write a message to the database
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("produtos");
 
-        minhaReferencia.child("produtos").child("001").child("descricao").setValue("coca-cola");
+        Produtos p = new Produtos();
+        p.setNome("Ovomaltine");
+        p.setPreco(15.00);
+
+        myRef.child("777").setValue(p);
+
+        /*minhaReferencia.child("produtos").child("001").child("descricao").setValue("coca-cola");
         minhaReferencia.child("produtos").child("001").child("preco").setValue("12.50");
 
         minhaReferencia.child("produtos").child("002").child("descricao").setValue("suco");
@@ -26,6 +36,6 @@ public class MainActivity extends AppCompatActivity {
         minhaReferencia.child("produtos").child("003").child("preco").setValue("6.50");
 
         minhaReferencia.child("produtos").child("004").child("descricao").setValue("café");
-        minhaReferencia.child("produtos").child("004").child("preco").setValue("7.90");
+        minhaReferencia.child("produtos").child("004").child("preco").setValue("7.90");*/
     }
 }
---
+++ Iniciando firebase/Atividade/src/main/java/com/example/myapplication/Produtos.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+public class Produtos {
+    private String nome;
+    private Double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
